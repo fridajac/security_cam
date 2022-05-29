ff1fe987399716e4cb25be2e867eb6e28688cd63
Frida Jacobsson <frida_jacobsson@live.se>
2022-05-29 16:22:48 +0200

    Implemented functionality for detecting movement in gray color

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import cv2
 
 cam = cv2.VideoCapture(0);
 while cam.isOpened():
-    ret, frame = cam.read()
+    ret, frame1 = cam.read()
+    ret, frame2 = cam.read()
+    diff = cv2.absdiff(frame1, frame2)
+    gray = cv2.cvtColor(diff, cv2.COLOR_RGBA2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
     if cv2.waitKey(10) == ord('q'):
         break;
-    cv2.imshow('My Own Cam', frame)
+    cv2.imshow('My Own Cam', diff)
